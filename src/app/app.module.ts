8b3374340082447399b17369878daeec72d33fa7
Rafael Camelo <rafaelcamelo31@gmail.com>
2021-10-10 20:09:27 -0400

    add routing module and additional styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, DevicesComponent],
+  declarations: [AppComponent, DevicesComponent, DeviceDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
